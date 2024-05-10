f800eb02158e89101d0bad4af4ba42e549766a6c
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-10 13:32:55 -0700

    updated

--- src/components/Navbar/NavPhone/NavPhone.jsx
+++ src/components/Navbar/NavPhone/NavPhone.jsx
@@ -2,8 +2,8 @@ import "./navphone.css";
 
 export function NavPhone() {
   return (
-    <div className="np-main-container">
-      <h1>nav phone</h1>
+    <div className="np-main-container m-0 green-bg">
+      <h1 className="lato">nav phone</h1>
     </div>
   );
 }
--- src/pages/LandingPage/LpPhone/LpPhone.jsx
+++ src/pages/LandingPage/LpPhone/LpPhone.jsx
@@ -1,8 +1,8 @@
 import "./lpphone.css";
 export function LpPhone() {
   return (
-    <div className="lpp-main-container">
-      <h1>Lp Phone</h1>
+    <div className="lpp-main-container m-0">
+      <h1 className="play blue-bg">Lp Phone</h1>
     </div>
   );
 }
