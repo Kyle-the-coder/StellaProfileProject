00bc76a0d177bf6a67f7d681300c620e7da1ab37
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-11 10:16:08 -0700

    updated

--- src/pages/LandingPage/LpMonitor/LpMonitor.jsx
+++ src/pages/LandingPage/LpMonitor/LpMonitor.jsx
@@ -2,22 +2,23 @@ import "./lpmonitor.css";
 export function LpMonitor() {
   return (
     <div className="lpm-main-container ">
-      <section className="lpm-hero-section"></section>
-      <div className="lpm-text-container">
-        <h1 className="play lpm-hero-text purple-font m0 f4">
-          <span className="yellow-font">C</span>osmos{" "}
-          <span className="purple-font">C</span>reations
-        </h1>
-        <p className="lpm-hero-text lato m0  f1-5">
-          <span className="yellow-font">
-            Welcome to Stella's World of 3D Wonder
-          </span>
-        </p>
-        <button className="lpm-cta-button">Explore My Work</button>
-      </div>
-      <div className="lpm-spline-container">
-        <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
-      </div>
+      <section className="lpm-hero-section">
+        <div className="lpm-text-container">
+          <h1 className="play lpm-hero-text purple-font m0 f4">
+            <span className="yellow-font">C</span>osmos{" "}
+            <span className="purple-font">C</span>reations
+          </h1>
+          <p className="lpm-hero-text mont m0  f1-5">
+            <span className="yellow-font">
+              Welcome to Stella's World of 3D Wonder
+            </span>
+          </p>
+          <button className="lpm-cta-button mont">Explore My Work</button>
+        </div>
+        <div className="lpm-spline-container">
+          <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
+        </div>
+      </section>
     </div>
   );
 }
