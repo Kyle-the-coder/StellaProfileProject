9bb601c03a9c6a614f17b6214fbad61758ed6ef8
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-11 10:01:31 -0700

    updated

--- src/pages/LandingPage/LpMonitor/LpMonitor.jsx
+++ src/pages/LandingPage/LpMonitor/LpMonitor.jsx
@@ -2,16 +2,18 @@ import "./lpmonitor.css";
 export function LpMonitor() {
   return (
     <div className="lpm-main-container ">
+      <section className="lpm-hero-section"></section>
       <div className="lpm-text-container">
-        <h1 className="play lpp-hero-text purple-font f4">
+        <h1 className="play lpm-hero-text purple-font m0 f4">
           <span className="yellow-font">C</span>osmos{" "}
           <span className="purple-font">C</span>reations
         </h1>
-        <p className="lpp-hero-text lato f1-5">
+        <p className="lpm-hero-text lato m0  f1-5">
           <span className="yellow-font">
             Welcome to Stella's World of 3D Wonder
           </span>
         </p>
+        <button className="lpm-cta-button">Explore My Work</button>
       </div>
       <div className="lpm-spline-container">
         <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
