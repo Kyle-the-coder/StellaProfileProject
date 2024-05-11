fa9418b25a7980b46eb0114b35e0bc63306fd361
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-10 23:26:11 -0700

    updated

--- src/pages/LandingPage/LpMonitor/LpMonitor.jsx
+++ src/pages/LandingPage/LpMonitor/LpMonitor.jsx
@@ -1,8 +1,19 @@
 import "./lpmonitor.css";
 export function LpMonitor() {
   return (
-    <div className="lpm-main-container">
-      <h1>Lp Monitor</h1>
+    <div className="lpm-main-container m-0">
+      <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
+      <div className="text-overlay">
+        <h1 className="play lpp-hero-text purple-font f4">
+          <span className="yellow-font">C</span>osmos{" "}
+          <span className="purple-font">C</span>reations
+        </h1>
+        <p className="lpp-hero-text lato f1-5">
+          <span className="yellow-font">
+            Welcome to Stella's World of 3D Wonder
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/LandingPage/LpPhone/LpPhone.jsx
+++ src/pages/LandingPage/LpPhone/LpPhone.jsx
@@ -2,7 +2,18 @@ import "./lpphone.css";
 export function LpPhone() {
   return (
     <div className="lpp-main-container m-0">
-      <h1 className="play purple-font">Lp Phone</h1>
+      <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
+      <div className="text-overlay">
+        <h1 className="play lpp-hero-text purple-font f4">
+          <span className="yellow-font">C</span>osmos{" "}
+          <span className="purple-font">C</span>reations
+        </h1>
+        <p className="lpp-hero-text lato f1-5">
+          <span className="yellow-font">
+            Welcome to Stella's World of 3D Wonder
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
